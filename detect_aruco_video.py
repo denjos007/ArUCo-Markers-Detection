9d0b3cf8e5d3b04ae2e115485750c7f03f46f7f5
joseph <josephprincemathew@outlook.com>
2023-10-25 13:31:02 -0400

    Change Generator with new implementation

--- detect_aruco_video.py
+++ detect_aruco_video.py
@@ -12,51 +12,56 @@ import cv2
 import sys
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--camera", required=True, help="Set to True if using webcam")
-ap.add_argument("-v", "--video", help="Path to the video file")
-ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="Type of ArUCo tag to detect")
-args = vars(ap.parse_args())
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--camera", required=True, help="Set to True if using webcam")
+    ap.add_argument("-v", "--video", help="Path to the video file")
+    ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="Type of ArUCo tag to detect")
+    args = vars(ap.parse_args())
 
-if args["camera"].lower() == "true":
-	video = cv2.VideoCapture(0)
-	time.sleep(2.0)
-	
-else:
-	if args["video"] is None:
-		print("[Error] Video file location is not provided")
-		sys.exit(1)
+    if args["camera"].lower() == "true":
+        video = cv2.VideoCapture(0)
+        time.sleep(2.0)
 
-	video = cv2.VideoCapture(args["video"])
+    else:
+        if args["video"] is None:
+            print("[Error] Video file location is not provided")
+            sys.exit(1)
 
-if ARUCO_DICT.get(args["type"], None) is None:
-	print(f"ArUCo tag type '{args['type']}' is not supported")
-	sys.exit(0)
+        video = cv2.VideoCapture(args["video"])
 
-arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
-arucoParams = cv2.aruco.DetectorParameters_create()
+    if ARUCO_DICT.get(args["type"], None) is None:
+        print(f"ArUCo tag type '{args['type']}' is not supported")
+        sys.exit(0)
 
-while True:
-	ret, frame = video.read()
-	
-	if ret is False:
-		break
+    aruco_dict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[args["type"]])
+    aruco_params = cv2.aruco.DetectorParameters()
+    aruco_detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_params)
+
+    while True:
+        ret, frame = video.read()
+
+        if ret is False:
+            break
+
+        h, w, _ = frame.shape
+
+        width = 1000
+        height = int(width * (h / w))
+        frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
+        corners, ids, rejected = aruco_detector.detectMarkers(frame)
+
+        detected_markers = aruco_display(corners, ids, rejected, frame)
+
+        cv2.imshow("Image", detected_markers)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    video.release()
 
 
-	h, w, _ = frame.shape
-
-	width=1000
-	height = int(width*(h/w))
-	frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
-	corners, ids, rejected = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
-
-	detected_markers = aruco_display(corners, ids, rejected, frame)
-
-	cv2.imshow("Image", detected_markers)
-
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-	    break
-
-cv2.destroyAllWindows()
-video.release()
+if __name__ == '__main__':
+    main()
